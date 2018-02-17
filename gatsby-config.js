2b10f09ee1cde03fa54d07c94c0c1225d5e7ef53
bobinlee <cultist_tp@naver.com>
2018-02-18 00:33:46 +0900

    rewrite scripts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
     "gatsby-transformer-remark",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
