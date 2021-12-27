4b992a7ae5e0ea6657d55970d6ae30b675980768
BoBeenLee <globaldev@naver.com>
2021-12-27 16:30:28 +0900

    fix: 테스트 깨짐 수정

--- jest.config.js
+++ jest.config.js
@@ -2,25 +2,24 @@
 const path = require(`path`);
 
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>src/setupTests.js"],
+  testEnvironment: "node",
+  setupFiles: ["<rootDir>/src/__tests__/jest.globals.ts"],
   moduleDirectories: ["node_modules", path.resolve(__dirname)],
-  moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.ts"
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`
+    "^.+\\.(tsx|ts)?$": "ts-jest"
   },
-  testRegex: "(.*(test|spec))\\.(jsx?|tsx?)$",
   testPathIgnorePatterns: [`node_modules`, `.cache`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
+    __PATH_PREFIX__: ``,
     "ts-jest": {
-      babelConfig: ".babelrc"
-    },
-    __PATH_PREFIX__: ``
+      tsConfig: "tsconfig.jest.json"
+    }
   },
-  testURL: `http://localhost`
+  testURL: `http://localhost`,
+  testRegex: "(.*(test|spec))\\.(jsx?|tsx?)$",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "src/__tests__/fileMock.js"
+  }
 };
