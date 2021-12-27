675046aefed1bcdf6170b2cec8865a386d639d75
BoBeenLee <globaldev@naver.com>
2021-12-27 16:39:24 +0900

    fix: filemock ts로 변경

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,6 @@ module.exports = {
   testRegex: "(.*(test|spec))\\.(jsx?|tsx?)$",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "src/__tests__/fileMock.js"
+      "src/__tests__/fileMock.ts"
   }
 };
